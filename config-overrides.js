9d79463feb0d6eee2a9d301c183bd61fa3c22c2a
Lucien Lee <lucien@linkernetworks.com>
2018-06-13 12:43:35 +0800

    filter ts rule and add exclude to prevent runtime parse error

--- config-overrides.js
+++ config-overrides.js
@@ -3,8 +3,25 @@ const tsImportPluginFactory = require('ts-import-plugin')
 const rewireCssModules = require('react-app-rewire-css-modules');
 const rewireLess = require('react-app-rewire-less');
 
+const ruleChildren = (loader) => loader.use || loader.oneOf || Array.isArray(loader.loader) && loader.loader || []
+
+const findIndexAndRules = (rulesSource, ruleMatcher) => {
+    let result = undefined
+    const rules = Array.isArray(rulesSource) ? rulesSource : ruleChildren(rulesSource)
+    rules.some((rule, index) => result = ruleMatcher(rule) ? {index, rules} : findIndexAndRules(ruleChildren(rule), ruleMatcher))
+    return result
+}
+
+const findRule = (rulesSource, ruleMatcher) => {
+    const {index, rules} = findIndexAndRules(rulesSource, ruleMatcher)
+    return rules[index]
+}
+
+const tsRuleMatcher = (rule) => rule.test && String(rule.test) === String(/\.(ts|tsx)$/);
+
 module.exports = function override(config, env) {
   // Load Antd on demand
+  const tsRule = findRule(config.module.rules, tsRuleMatcher)
   const tsLoader = getLoader(
     config.module.rules,
     rule =>
@@ -22,6 +39,8 @@ module.exports = function override(config, env) {
       })]
     })
   };
+
+  tsRule.exclude = /node_modules/
 
   // Customize Antd Theme
   config = rewireLess.withLoaderOptions({
