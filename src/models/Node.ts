1c1a02ae84eba7fd7d40cf2a3e6931d75c651d0b
yunchen <yunchen@nisra.net>
2018-07-20 17:29:09 +0800

    Add node model

--- src/models/Node.ts
+++ src/models/Node.ts
@@ -1,8 +1,39 @@
+export interface Detail {
+  hostname: string;
+  createAt: number;
+  status: string;
+  os: string;
+  kernelVersion: string;
+  kubeproxyVersion: string;
+  kubernetesVersion: string;
+  label: Map<string, string>;
+}
+
+export interface Resource {
+  cpuRequests: number;
+  cpuLimits: number;
+  memoryRequests: number;
+  memoryLimits: number;
+  allocatableCPU: number;
+  allocatableMemory: number;
+  allocatablePods: number;
+  allocatableEphemeralStorage: number;
+  capacityCPU: number;
+  capacityMemory: number;
+  capacityPods: number;
+  capacityEphemeralStorage: number;
+}
+
 export interface NetworkInterfaceController {
   name: string;
   default: boolean;
   type: NICType;
   pciID: string;
+}
+
+export interface Info {
+  detail: Detail;
+  resource: Resource;
 }
 
 export enum NICType {
