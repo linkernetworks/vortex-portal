fad7fc755c3d8d28221af68bb5cd28849372776d
yunchen <yunchen@nisra.net>
2018-08-02 17:46:39 +0800

    Add cluster model

--- src/store/ducks/cluster/index.ts
+++ src/store/ducks/cluster/index.ts
@@ -4,11 +4,13 @@ import {
   ClusterActionType
 } from './reducers';
 
+import * as clusterModels from './models';
 import * as clusterActions from './actions';
 import * as clusterOperations from './operations';
 import * as clusterSelectors from './selectors';
 
 export {
+  clusterModels,
   clusterActions,
   clusterOperations,
   clusterSelectors,
