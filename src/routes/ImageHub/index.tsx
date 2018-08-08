2f432dca95699f3ec3e040ae6b4ef8074925cfea
Lucien Lee <lucien@linkernetworks.com>
2018-08-08 19:08:39 +0800

    fix clear localstorage in wrong condition

--- src/routes/ImageHub/index.tsx
+++ src/routes/ImageHub/index.tsx
@@ -89,13 +89,12 @@ class ImageHub extends React.PureComponent<HubProps, HubState> {
         }),
         onClose: this.props.resetError()
       });
+      localStorage.removeItem(REGISTRY_TOKEN);
     }
 
     if (!prevProps.isAuth && this.props.isAuth) {
       localStorage.setItem(REGISTRY_TOKEN, this.props.token);
       this.props.fetchImagesData();
-    } else {
-      localStorage.removeItem(REGISTRY_TOKEN);
     }
   }
 
