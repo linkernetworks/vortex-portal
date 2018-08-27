65d5eb5aaa5dbc19ceb7ec412a8b6d4692c319d6
Lucien Lee <lkiral7903@gmail.com>
2018-08-28 02:55:44 +0800

    fix #17

--- src/services/request.ts
+++ src/services/request.ts
@@ -3,14 +3,16 @@ import { loadToken } from '@/utils/auth';
 
 axios.interceptors.request.use(
   config => {
-    const token = loadToken();
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    const { url } = config;
+    if (url && /^\/v1\/(?!registry\/).*/.test(url)) {
+      const token = loadToken();
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
     }
     return config;
   },
   error => {
-    // Do something with request error
     return Promise.reject(error);
   }
 );
