fe39ac4167973fa651e1f8c29ad672f8913eb2a7
Lucien Lee <lkiral7903@gmail.com>
2018-10-02 17:51:37 +0800

    hotfix: terminal layout calc too early

--- src/components/ModalTerminal/index.tsx
+++ src/components/ModalTerminal/index.tsx
@@ -5,6 +5,8 @@ import * as styles from './styles.module.scss';
 
 import ExecTerminal from '@/components/ExecTerminal';
 
+// From Antd motion theme
+const duration = 300;
 interface ModalTerminalProps {
   title: string;
   welcomeMsg?: string;
@@ -16,29 +18,56 @@ interface ModalTerminalProps {
   onCloseModal: () => void;
 }
 
-class ModalTerminal extends React.PureComponent<ModalTerminalProps, object> {
+interface ModalTerminalState {
+  readyToLoadTerm: boolean;
+}
+
+class ModalTerminal extends React.PureComponent<
+  ModalTerminalProps,
+  ModalTerminalState
+> {
+  public state = {
+    readyToLoadTerm: false
+  };
+
+  public componentDidUpdate(prevProps: ModalTerminalProps) {
+    if (!prevProps.execIdentifier && this.props.execIdentifier) {
+      setTimeout(() => {
+        this.setState({ readyToLoadTerm: true });
+      }, duration);
+    }
+  }
+
+  protected handleClose = () => {
+    this.props.onCloseModal();
+    this.setState({ readyToLoadTerm: false });
+  };
+
   public render() {
     const { execIdentifier, title, welcomeMsg, onCloseModal } = this.props;
+    const { readyToLoadTerm } = this.state;
+
     return (
       <Modal
         visible={!!execIdentifier}
         title={title}
         className={styles['terminal-modal']}
-        onCancel={onCloseModal}
+        onCancel={this.handleClose}
         footer={null}
         width={960}
-        bodyStyle={{ padding: 0, height: '65vh' }}
+        bodyStyle={{ padding: 0, height: '65vh', background: 'black' }}
         destroyOnClose={true}
       >
-        {execIdentifier && (
-          <ExecTerminal
-            namespace={execIdentifier.namespace}
-            podName={execIdentifier.podName}
-            containerName={execIdentifier.containerName}
-            welcomeMsg={welcomeMsg}
-            onClose={onCloseModal}
-          />
-        )}
+        {readyToLoadTerm &&
+          execIdentifier && (
+            <ExecTerminal
+              namespace={execIdentifier.namespace}
+              podName={execIdentifier.podName}
+              containerName={execIdentifier.containerName}
+              welcomeMsg={welcomeMsg}
+              onClose={onCloseModal}
+            />
+          )}
       </Modal>
     );
   }
